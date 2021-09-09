cdbe48b557cd6ae65b2f46862d1cc6da0d404349
Phasamon <52498379+phasamon-p@users.noreply.github.com>
2021-09-10 01:26:48 +0700

    Add Project

---
+++ register_db.php
@@ -0,0 +1,59 @@
+<?php 
+    session_start();
+    
+    include('server.php');
+    $errors = array();
+
+    if (isset($_POST['reg_user'])) {
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password_1 = mysqli_real_escape_string($conn, $_POST['password_1']);
+        $password_2 = mysqli_real_escape_string($conn, $_POST['password_2']);
+        $my_arr = array('name'=>$name, 'lastname'=>$lastname, 'username'=>$username, 'email'=>$email, 'password_1'=>$password_1, 'password_2'=>$password_2);
+        if (empty($username)) {
+            array_push($errors, "Username is required");
+            $_SESSION['error'] = "Username is required";
+        }
+        if (empty($email)) {
+            array_push($errors, "Email is required");
+            $_SESSION['error'] = "Email is required";
+        }
+        if (empty($password_1)) {
+            array_push($errors, "Password is required");
+            $_SESSION['error'] = "Password is required";
+        }
+        if ($password_1 != $password_2) {
+            array_push($errors, "The two passwords do not match");
+            $_SESSION['error'] = "The two passwords do not match";
+        }
+
+        
+
+        $user_check_query = "SELECT * FROM user WHERE username = '$username' OR email = '$email' LIMIT 1";
+        $query = mysqli_query($conn, $user_check_query);
+        $result = mysqli_fetch_assoc($query);
+        if ($result) { // if user exists
+            if ($result['username'] === $username) {
+                array_push($errors, "Username already exists");
+            }
+            if ($result['email'] === $email) {
+                array_push($errors, "Email already exists");
+            }
+        }
+        if (count($errors) == 0) {
+            $password = md5($password_1);
+
+            $sql = "INSERT INTO user (username, email, password, name, lname, permission) VALUES ('$username', '$email', '$password', '$name', '$lastname', 0)";
+            mysqli_query($conn, $sql);
+
+            // $_SESSION['username'] = $username;
+            // $_SESSION['success'] = "You are now logged in";
+            header('location: index.php');
+        } else {
+            header("location: register.php");
+        }
+    }
+
+?>
